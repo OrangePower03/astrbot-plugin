0a3859d00a0180b4cc94af9b988a7c4ddaf8c62f
OrangePower03 <1697081049@qq.com>
2025-08-10 02:16:11 +0800

    test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
@@ -19,6 +20,13 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    filter.command("test")
+    async def test(self, event: AstrMessageEvent):
+        url = "http://backend:8080/image/test"
+        res = requests.get(url=url)
+        yield event.plain_result(res.text)  # 发送一条纯文本消息
+
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
