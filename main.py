76895a30477ac27467ecf0ac805a81108b7820fe
OrangePower03 <1697081049@qq.com>
2025-08-10 13:56:08 +0800

    test

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
+
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
+from astrbot.core.message.components import *
 
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
@@ -25,17 +27,35 @@ class MyPlugin(Star):
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")  # 发送一条纯文本消息
 
-    # @filter.command("test")
-    # async def test(self, event: AstrMessageEvent):
-    #     url = "http://backend:8080/image/test"
-    #     res = requests.get(url=url)
-    #     yield event.plain_result(res.text)  # 发送一条纯文本消息
+    @filter.command("test")
+    async def test(self, event: AstrMessageEvent):
+        chain = event.get_messages()
+        yield event.plain_result(f"一共有{len(chain)}条数据链,数据是:{event.get_message_outline()}")
+        # for i in chain:
+        #     if isinstance(i, Plain):
+
+
+
+    @filter.command("send_image")
+    async def send_image(self, event: AstrMessageEvent):
+        yield event.image_result("https://cq-note.oss-cn-hangzhou.aliyuncs.com/20250425232807.png")
 
     @filter.command("添加词条")
     async def addDict(self, event: AstrMessageEvent):
         url = self.base_url + "/image/addDict"
         body = {
             "dict": event.message_str.removeprefix("添加词条").strip()
+        }
+        res = requests.post(url=url, json=body)
+        yield event.plain_result(res.text)
+
+
+    @filter.command("添加图片")
+    async def addPic(self, event: AstrMessageEvent):
+        url = self.base_url + "/image/addPic"
+        # event.get_extra()
+        body = {
+            "dict": event.message_str.removeprefix("添加图片").strip()
         }
         res = requests.post(url=url, json=body)
         yield event.plain_result(res.text)
