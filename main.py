ba50201e53f92daee070af0ced32a97d371b6013
OrangePower03 <1697081049@qq.com>
2025-08-10 16:35:57 +0800

    说明书

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from astrbot.api import logger
 from astrbot.core.message.components import *
 
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+@register("image", "Charlie", "一个简单的图片添加插件", "1.0.0")
 class MyPlugin(Star):
 
     def __init__(self, context: Context):
@@ -18,19 +18,39 @@ class MyPlugin(Star):
         pass
 
     # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
-        """这是一个 hello world 指令"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-        user_name = event.get_sender_name()
-        message_str = event.message_str  # 用户发的纯文本消息字符串
-        message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
-        logger.info(message_chain)
-        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")  # 发送一条纯文本消息
+    # @filter.command("helloworld")
+    # async def helloworld(self, event: AstrMessageEvent):
+    #     """这是一个 hello world 指令"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+    #     user_name = event.get_sender_name()
+    #     message_str = event.message_str  # 用户发的纯文本消息字符串
+    #     message_chain = event.get_messages()  # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+    #     logger.info(message_chain)
+    #     yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")  # 发送一条纯文本消息
 
     # @filter.command("test")
     # async def test(self, event: AstrMessageEvent):
     #     pass
 
+    @filter.command("说明书")
+    async def specification(self, event: AstrMessageEvent):
+        res = f"""
+            首先如果不存在词条,需要先执行以下指令
+            /添加词条 [词条名]
+            注意:词条名前必须有空格隔开
+            
+            然后就可以添加图片了,执行以下指令
+            [引用图片]
+            /添加图片 [词条名]
+            
+            注意:不能引用除了图片以外的数据,引用别人引用的图片也不行
+                不能在引用图片后不能at人,否则添加失败,即不能
+                [引用图片]
+                @xxx /添加图片 [词条名]
+                
+            最后就可以获取随机一张图片了,执行以下指令
+            /来只 [词条名]
+        """
+        yield event.plain_result(res)
 
 
 
