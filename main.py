80ab5c79a40d9746c20e9bc579c1487aca61505f
OrangePower03 <1697081049@qq.com>
2025-08-18 20:56:28 +0800

    test

--- main.py
+++ main.py
@@ -27,9 +27,6 @@ class MyPlugin(Star):
     #     logger.info(message_chain)
     #     yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!")  # 发送一条纯文本消息
 
-    # @filter.command("test")
-    # async def test(self, event: AstrMessageEvent):
-    #     pass
 
     @filter.command("说明书")
     async def specification(self, event: AstrMessageEvent):
@@ -118,6 +115,15 @@ class MyPlugin(Star):
             yield event.image_result(res.text)
         else:
             yield event.plain_result(f"出现错误,错误信息:{res.text}")
+
+    @filter.command("展示词条")
+    async def show_dict(self, event: AstrMessageEvent):
+        url = self.base_url + f"/image/showDict"
+        res = requests.post(url=url)
+        if res.status_code == 200:
+            return res.text
+        logger.error(f"展示词条时服务器错误,错误码:{res.json()}")
+        return "服务器错误"
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
