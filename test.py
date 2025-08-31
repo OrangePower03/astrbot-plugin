a1aaed7b5d6be082bd7dd202413745ac7d21d9db
OrangePower03 <1697081049@qq.com>
2025-08-31 23:43:08 +0800

    1

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
 # https://github.com/OrangePower03/astrbot-plugin
-from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
-from astrbot.api.star import Context, Star, register
-from astrbot.api import logger
-from astrbot.core.message.components import *
-
-
-@register("test", "Charlie", "一个简单的测试插件", "1.0.0")
-class TestPlugin(Star):
-    def __init__(self, context: Context):
-        super().__init__(context)
-
-    @filter.command("t")
-    async def test(self, event: AstrMessageEvent):
-        if event.is_wake:
-            yield event.plain_result(event.get_message_outline())
