ab7ed360f50298c766fe0a8282b5c6795fe042ad
OrangePower03 <1697081049@qq.com>
2025-08-31 23:30:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -209,7 +209,7 @@ class CommonPlugin(Star):
 
     def __init__(self, context: Context):
         super().__init__(context)
-        TestPlugin(context)
+        TestPlugin(super().context)
         self.specification = """
         /说明书 [指令功能]
         指令功能: 图片/聊天记录/题库
